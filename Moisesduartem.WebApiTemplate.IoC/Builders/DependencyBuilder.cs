298038831e41127816d27b1d3f97d755709ea29e
Moisés Maia <moisesduartem@gmail.com>
2022-09-10 17:31:06 -0300

    feat(ioc): configure fv, mediatr, versioning, automapper

---
+++ Moisesduartem.WebApiTemplate.IoC/Builders/DependencyBuilder.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Moisesduartem.WebApiTemplate.IoC.Builders
+{
+    public class DependencyBuilder
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceCollection _services;
+
+        public DependencyBuilder(IConfiguration configuration, IServiceCollection services)
+        {
+            _configuration = configuration;
+            _services = services;
+        }
+
+        public DependencyBuilder AddAutoMapper()
+        {
+            _services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            return this;
+        }
+
+        public DependencyBuilder AddFluentValidation()
+        {
+            _services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+            return this;
+        }
+
+        public DependencyBuilder AddMediatR()
+        {
+            _services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+            return this;
+        }
+    }
+}
---
+++ Moisesduartem.WebApiTemplate.IoC/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moisesduartem.WebApiTemplate.IoC.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moisesduartem.WebApiTemplate.IoC.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureConfiguration(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            new DependencyBuilder(configurationManager, services)
+                .AddAutoMapper()
+                .AddMediatR()
+                .AddFluentValidation();
+        }
+    }
+}
