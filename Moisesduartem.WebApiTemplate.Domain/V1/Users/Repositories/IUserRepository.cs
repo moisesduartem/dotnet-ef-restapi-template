442da143eae32b8a12913e8595e092e57e9314a5
Moisés Maia <moisesduartem@gmail.com>
2022-09-10 18:47:39 -0300

    feat(login): user repo (in mem, for now)

---
+++ Moisesduartem.WebApiTemplate.Domain/V1/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Moisesduartem.WebApiTemplate.Domain.V1.Shared;
+using Moisesduartem.WebApiTemplate.Domain.V1.Users.Entities;
+
+namespace Moisesduartem.WebApiTemplate.Domain.V1.Users.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
---
+++ Moisesduartem.WebApiTemplate.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Moisesduartem.WebApiTemplate.Domain.V1.Users.Entities;
+using Moisesduartem.WebApiTemplate.Domain.V1.Users.Repositories;
+
+namespace Moisesduartem.WebApiTemplate.Infra.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User?> GetByEmailAsync(string email)
+        {
+            var user = new User("Mr. User", "user@email.com", "username", "password123");
+            return Task.FromResult<User?>(user);
+        }
+    }
+}
