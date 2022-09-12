7634aa7be1498a2664226aa39b42de6ce2ed0571
Moisés Maia <moisesduartem@gmail.com>
2022-09-12 16:56:29 -0300

    feat: create identity context

---
+++ RestApi.Identity/Data/ApplicationIdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestApi.Identity.Data
+{
+    public class ApplicationIdentityContext : IdentityDbContext
+    {
+        public ApplicationIdentityContext(DbContextOptions<ApplicationIdentityContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
